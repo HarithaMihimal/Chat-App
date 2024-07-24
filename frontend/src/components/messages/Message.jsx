5a085c17511073e73453e4e49b82a325e348b785
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-07-24 21:06:24 +0530

    Update Message.jsx

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -17,13 +17,17 @@ const Message = ({ message }) => {
 	return (
 		<div className={`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
-				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
-				</div>
+			<div className='w-10 rounded-full'>
+				<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 			</div>
-			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
-			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
-		</div>
+			</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>
+				{message.message}
+			</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>
+				{formattedTime}
+			</div>
+			</div>
 	);
 };
 export default Message;

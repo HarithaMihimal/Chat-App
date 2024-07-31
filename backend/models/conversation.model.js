2b1d68d267009debe7d8771b68552b941f96556d
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-07-31 23:31:35 +0530

    Update conversation.model.js

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -21,7 +21,5 @@ const conversationSchema = new mongoose.Schema(
 	},
 	{ timestamps: true }
 );
-
 const Conversation = mongoose.model("Conversation", conversationSchema);
-
 export default Conversation;

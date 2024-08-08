e32e3bd79ac67c63bb974eca53344a458350b8a1
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-08-08 22:27:48 +0530

    Update Conversation.jsx

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -11,8 +11,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 	return (
 		<>
 		<div
-			className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer${isSelected ? "bg-sky-500" : ""}
-			`}
+			className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer${isSelected ? "bg-sky-500" : ""}`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
 				<div className={`avatar ${isOnline ? "online" : ""}`}>

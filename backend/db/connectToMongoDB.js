72fd4601fdaa91eb198292ff65ea645a97071c7f
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-10-04 23:51:27 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -4,7 +4,7 @@ const connectToMongoDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_DB_URI);
 		console.log("Connected to MongoDB");
-	} catch (error) {
+	}      catch (error) {
 		
 		console.log("Error connecting to MongoDB", error.message);
 	}
